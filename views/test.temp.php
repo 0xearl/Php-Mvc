ec15efe2c4be3257d0fc041f9be61c76c3f80525
Earl Sabalo <earljohnsabalo@gmail.com>
2024-03-29 02:33:12 +0800

    new folder for views

---
+++ views/test.temp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Another Test</title>
+</head>
+<body>
+    Hello <?php echo $_GET['name'] ?? ''; ?>
+</body>
+</html>
