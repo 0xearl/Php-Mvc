521eac65e5121d5ede8afb68a187226a0a2870a0
Earl Sabalo <earljohnsabalo@gmail.com>
2024-03-30 01:32:52 +0800

    cleaned index.php

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
 <?php
-include_once('autoload.php');
-include_once('./helpers.php');
 
-use App\Route;
-use App\Controllers\Index;
-use App\Controllers\Test;
-/**
- * @author Earl Sabalo
- * 
- * This is the file responsible for creating new routes without creating a controller.
- * 
- * ex: 
- * $route->get('/test', function() { 
- *    return 'test'; 
- * });
- */
-
-$routes = new Route();
-
-$routes->get('/hello-world', function() {
-    return 'Hello World';
-});
-
-$routes->get('/', [Index::class, 'index']);
-$routes->get('/index/test', [Index::class, 'test']);
-$routes->get('/test', [Test::class, 'index']);
-$routes->resource('/resource', App\Controllers\Resource::class);
-
-$routes->loadRoutes();
+include_once('./bootloader.php');
