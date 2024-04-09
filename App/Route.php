95c11cabbbc219fd94d5af3ac9c0a28523d1856d
Earl Sabalo <earljohnsabalo@gmail.com>
2024-04-10 00:15:55 +0800

    Added Middleware checking before going to the route | made match() & getMiddleware() into a private function

--- App/Route.php
+++ App/Route.php
@@ -33,22 +33,36 @@ class Route extends WebRouter {
 
         $matched_route = $this->match();
 
+        $middleware = $this->getMiddleware();
+
         if ( $matched_route === null ) {
             return response(404);
         }
 
         if ( $matched_route[1] ) {
-            
-            return call_user_func( $matched_route[0], $matched_route[1] );
+
+            if ( $middleware ) {
+                if ( $middleware::call() ) {
+                    return call_user_func_array( $matched_route[0], $matched_route[1] );
+                }
+            } else {
+                return call_user_func_array( $matched_route[0], $matched_route[1] );
+            }
 
         } else {
 
-            return call_user_func( $matched_route[0], new Request() );
+            if ( $middleware ) {
+                if ( $middleware::call() ) {
+                    return call_user_func( $matched_route[0], new Request() );
+                }
+            } else {
+                return call_user_func( $matched_route[0], new Request() );
+            }
             
         }
     }
 
-    public function match()
+    private function match()
     {
         if (!isset($this->routes[$this->request_method])) {
             return null;
@@ -58,11 +72,30 @@ class Route extends WebRouter {
             $matchResult = preg_match('/^' . $route . '$/', $this->uri, $matches);
             if ($matchResult) {
                 array_shift($matches); // Remove the first element from $matches
-                $param = reset($matches);
+                $param = array_filter($matches, 'is_int', ARRAY_FILTER_USE_KEY);
                 return [$action, $param];
             }
         }
 
         return null;
+    }
+
+
+    /**
+     * This function gets the middleware of the route if it exists.
+     * 
+     * @return App\Interfaces\MiddlewareInterface|false
+     */
+    private function getMiddleware()
+    {
+        foreach ( $this->middlewares as $route => $middleware ) {
+
+            $matchResult = preg_match('/^' . $route . '$/', $this->uri);
+            if ( $matchResult ) {
+                return $middleware;
+            }
+
+            return false;
+        }
     }
 }
